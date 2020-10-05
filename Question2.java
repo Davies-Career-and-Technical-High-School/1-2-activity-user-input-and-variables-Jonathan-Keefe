41350e772d5f11d571c6e11c59ad5efae71a6e0f
Jonathan Keefe <jonathan.keefe@daviestech.org>
2020-10-05 15:27:03 +0000

    Question 2

--- Question2.java
+++ Question2.java
@@ -1,11 +1,21 @@
 /* Activity 1.2 - Question 2 */
-
+import java.util.Scanner;
 
 
 class Question2 {
     public static void main(String[] args) {
 
         /* Write your code here */
-
+      Scanner scan = new Scanner(System.in);
+      String name1;
+      String name2;
+      String name3;
+      System.out.println("Give me a name");
+      name1 = scan.nextLine();
+      System.out.println("Give me a name");
+      name2 = scan.nextLine();
+      System.out.println("Give me a name");
+      name3 = scan.nextLine();
+      System.out.println( name1 + " " + name2 + " " + name3);
     }
 }
